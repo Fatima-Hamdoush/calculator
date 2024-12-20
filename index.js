3288a2208d0c5c052b52d3ebff3287f6f639f0b6
Fatima Hamdouch <hamdouchfatima168@gmail.com>
2024-12-20 10:23:55 +0200

    JS <> Implemented  the dark mode

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
-const val = document.getElementById('diaplay')
-function prefixEval(expression) {
-  let temp = expression.split(' ')
+const lune = document.getElementById('lune')
+ const calculator=document.getElementById('calculator')
+const a =0;
+ let stackprofix = []
+ let stack = []
+let counter=0
+
+
+function prefixEval() {
+    let val = document.getElementById('display').value
+  let temp = val.split(' ')
   let expr = temp.reverse()
-  let stack = []
 
   for (let i = 0; i < expr.length; i++) {
     if (
@@ -14,14 +21,15 @@ function prefixEval(expression) {
     ) {
       let j = stack.pop()
       let k = stack.pop()
-     let temp = checkOperator(parseFloat(j), parseFloat(k), expr[i])
-
+      let temp = checkOperator(parseFloat(j), parseFloat(k), expr[i])
       stack.push(temp)
     } else {
       stack.push(expr[i])
     }
   }
-  return stack
+
+  document.getElementById('display').value = stack
+
 }
 
 function checkOperator(a, b, op) {
@@ -43,49 +51,58 @@ function checkOperator(a, b, op) {
   }
 }
 
-function evaluatePostfix(exp) {
-  //create a stack
-  let stack = []
+function Postfixeval() {
+  
+   val = document.getElementById('display').value
 
   // Scan all characters one by one
-  for (let i = 0; i < exp.length; i++) {
-    let c = exp[i]
+  for (let i = 0; i < val.length; i++) {
+    let c = val[i]
      if (c === ' ') continue;//ignore space
-
     // If the scanned character is an operand (number here),
     // push it to the stack.
-    if (!isNaN(parseInt(c))) stack.push(c.charCodeAt(0) - '0'.charCodeAt(0))
+    if (!isNaN(parseInt(c))) stackprofix.push(c.charCodeAt(0) - '0'.charCodeAt(0))
     //  If the scanned character is an operator, pop two
     // elements from stack apply the operator
     else {
-      let val1 = stack.pop()
-      let val2 = stack.pop()
-
+      let val1 = stackprofix.pop()
+      let val2 = stackprofix.pop()
       switch (c) {
         case '+':
-          stack.push(val2 + val1)
+          stackprofix.push(val2 + val1)
           break
-
         case '-':
-          stack.push(val2 - val1)
+          stackprofix.push(val2 - val1)
           break
-
         case '/':
-          stack.push(val2 / val1)
+          stackprofix.push(val2 / val1)
           break
-
         case '*':
-          stack.push(val2 * val1)
+          stackprofix.push(val2 * val1)
           break
       }
     }
   }
-  return stack.pop()
+  res = stackprofix.pop()
+  console.log(res)
+  document.getElementById('display').value = res
+  
+
 }
 
-console.log(prefixEval('+ 9 * 2 3'))
-console.log(prefixEval('+ 8 * 3 4'))
-let exp = '9 2 3 * +'
-console.log('postfix evaluation: ' + evaluatePostfix(exp))
+function darkmode()
+{  
+    
+    if (counter%2===0) {
+   calculator.style.backgroundColor ='rgb(200, 216, 230)'
+     lune.style.backgroundColor = 'rgb(200, 216, 230)'
+}
+else{
+    calculator.style.backgroundColor = 'rgb(58, 68, 82)'
+    lune.style.backgroundColor = 'rgb(58, 68, 82)'
+
+}
+   counter++;  
+}
 
 
